2ba97d8c0c64a03e695193b450670303f166134b
Beste Bayhan <bbayhan@outlook.com>
2024-11-20 18:18:19 +0300

    auth router has updated

--- backend/src/Routes/authRouter.js
+++ backend/src/Routes/authRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const { register, login } = require('../Services/authService');
+const UsersController = require('../Controllers/userController');
 
 const router = express.Router();
+const usersController = new UsersController(); // Instantiate UsersController
 
 // Registration Endpoint
 router.post('/register', async (req, res) => {
     try {
         const user = req.body; // Get user details from the request body
-        const result = await register(user);
+        const result = await usersController.register(user); // Call register method
         res.status(201).json(result);
     } catch (err) {
         res.status(400).json({ error: err.message });
@@ -18,7 +19,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body; // Get email and password from the request body
-        const userDetails = await login(email, password);
+        const userDetails = await usersController.login(email, password); // Call login method
         res.status(200).json(userDetails);
     } catch (err) {
         res.status(401).json({ error: err.message }); // Unauthorized
