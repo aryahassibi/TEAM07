48ce63a60656a0598d6a7b76a739d768af3ae76e
arya hassibi <aryahassibi@gmail.com>
2024-11-29 20:35:28 +0300

    refactor(Cart): use global button styles

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -17,7 +17,7 @@ const Cart = () => {
     <div className='cart-page'>
       <Navbar />
       <div style={{ padding: '20px' }}>
-        <h2>Your Cart</h2>
+        <h1>Your Cart</h1>
         {cartItems.length > 0 ? (
           <ul>
             <div>
@@ -34,15 +34,12 @@ const Cart = () => {
                   <p>Price per item: ${item.price}</p>
                   <p>Weight: {item.weight_grams}g</p> {/* Display weight */}
                   <div>
-                    <button onClick={() => decreaseQuantity(item.variantId)}>-</button>
-                    <span style={{ margin: '0 10px' }}>Quantity: {item.quantity}</span>
-                    <button onClick={() => increaseQuantity(item.variantId)}>+</button>
+                    <button className="button button-primary" onClick={() => decreaseQuantity(item.variantId)}>-</button>
+                    <span className="cart-item-quantity">Quantity: {item.quantity}</span>
+                    <button className="button button-primary" onClick={() => increaseQuantity(item.variantId)}>+</button>
                   </div>
                   <p>Total price for this item: ${(item.price * item.quantity).toFixed(2)}</p>
-                  <button
-                    onClick={() => removeFromCart(item.variantId)}
-                    style={{ color: 'red' }}
-                  >
+                  <button className="button button-danger remove-button" onClick={() => removeFromCart(item.variantId)}>
                     Remove from Cart
                   </button>
                 </div>
