fd7d918ee4ef7ffbdb7b9d5af4aa4a6e1dafd17a
arya hassibi <aryahassibi@gmail.com>
2024-11-30 22:07:02 +0300

    refactor(App.js): remove unused state and API call for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 // src/App.js
 
-import { useState, useEffect } from 'react'; // Import useState and useEffect
-import axios from 'axios';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage';
 import Cart from './components/Cart';
@@ -13,15 +11,6 @@ import { CartProvider } from './CartContext'; // Import CartProvider
 import './index.css'; 
 
 function App() {
-  const [products, setProducts] = useState([]); // Correctly define products and setProducts
-  
-  useEffect(() => {
-    // Fetch data from the backend API
-    axios.get('http://localhost:5000/api/products')
-      .then(response => setProducts(response.data)) // Use setProducts here
-      .catch(error => console.error('Error fetching products:', error));
-  }, []);
-
   // Use the products state, for example by passing it to a component
   return (
     <CartProvider> {/* Wrap the app with CartProvider */}
@@ -30,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/products" element={<ProductsPage products={products} />} /> 
+            <Route path="/products" element={<ProductsPage />} /> 
             <Route path="/product/:variant_id" element={<ProductDetail />} />
             <Route path="/loginregister" element={<LoginRegister/>} />
           </Routes>
