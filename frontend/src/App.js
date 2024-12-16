4ca2b12f3ae52efe3b51b8df4b6733d2c04c546e
Mustafa Topcu <mmstf.tpct@gmail.com>
2024-12-16 18:32:29 +0300

    add OrdersPage and SidebarLayout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,8 @@ import AdminLoginPage from './components/AdminLoginPage';
 import Checkout from './components/Checkout'; 
 import OrderFailed from './components/OrderFailed';
 import OrderSuccess from './components/OrderSuccess';
+import SidebarLayout from './components/SidebarLayout'; 
+import OrderPage from "./components/OrdersPage";
 import './index.css'; 
 
 function App() {
@@ -42,6 +44,7 @@ function App() {
             <Route path="/checkout" element={<Checkout />} />
             <Route path="/order-failed" element={<OrderFailed/>} />
             <Route path="/order-success" element={<OrderSuccess/>} />
+            <Route path="/my-orders" element={<SidebarLayout><OrderPage /></SidebarLayout>} />
           </Routes>
         </div>
       </Router>
