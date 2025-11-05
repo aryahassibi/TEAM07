9bde0fac78e5da42923b8d6ffa0912a67b6ec724
arya hassibi <aryahassibi@gmail.com>
2025-11-05 16:51:03 +0300

    Refactor AdminSalesMgmt buttons

--- frontend/src/components/AdminSalesMgmt.js
+++ frontend/src/components/AdminSalesMgmt.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 const AdminSalesMgmt = () => {
     
     const navigate = useNavigate();
-    
     return (
         <div className="admin-container">
             
@@ -14,12 +13,25 @@ const AdminSalesMgmt = () => {
                 Manage sales operations, invoices, and discounts.
             </p>
             <div className="sales-actions">
-                <button className="action-button">Set Prices/Discounts</button>
-                <button className="action-button">Notify Users About Discounts</button>
-                <button className="action-button" onClick={() => navigate("/invoice_list")}>View Invoices/Profit Chart</button>
-                <button className="action-button" onClick={() => navigate("/refund-list")}>Evaluate Refund Requests</button>
+                <button
+                    className="action-button"
+                    onClick={() => navigate("/admin/set_prices_discounts")}
+                >
+                    Set Prices/Discounts/Notify Users About Discounts
+                </button>
+                <button
+                    className="action-button"
+                    onClick={() => navigate("/admin/invoice_list")}
+                >
+                    View Revenue/Profit Chart
+                </button>
+                <button
+                    className="action-button"
+                    onClick={() => navigate("/admin/refund-list")}
+                >
+                    Evaluate Refund Requests
+                </button>
             </div>
-            
         </div>
     );
 };
